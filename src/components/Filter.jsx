b76d67d24a32f241afd7f3a0005d174405b8522e
Nabeel Akbar <studiosnabeel@gmail.com>
2023-05-28 16:42:55 +0500

    Add filter component

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -1,10 +1,8 @@
-import React from 'react';
-
 const Filter = ({ icon, title }) => {
   return (
     <div
       className="text-gray-500 font-thin mt-6 p-2 flex flex-col justify-center items-center gap-2
-    hover:border-b-2 hover:border-gray-400 hover:text-gray-900 active:border-b-2 active:border-black active:text-black"
+    hover:border-b-2 hover:border-gray-400 hover:text-gray-900 active:border-b-2 active:border-black active:text-black cursor-pointer"
     >
       <div>{icon}</div>
       <div className="">{title}</div>
